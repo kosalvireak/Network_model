122a8553daf798653fe3fa68c487831e02183c12
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-25 18:05:51 +0700

    add adjacency list

--- prim.py
+++ prim.py
@@ -1,42 +1,53 @@
 # https://stackabuse.com/courses/graphs-in-python-theory-and-implementation/lessons/minimum-spanning-trees-prims-algorithm/#whatisaminimumspanningtree
 
-#create a class name Graph
-class Graph: 
+# create a class name Graph
+class Graph:
     #this is constructor
-    def __init__(self, num_of_nodes): 
+    # In python we call init
+    def __init__(self, num_of_nodes):
         self.m_num_of_nodes = num_of_nodes
-        # create an empty adjacecy matrix with the nodes of 9 and all value equal to 0
-        self.m_graph = [[0 for column in range(num_of_nodes)] 
-                    for row in range(num_of_nodes)]
-    #this is method ,used to reduce the number of edge, 
-    def add_edge(self, node1, node2, weight): 
-        self.m_graph[node1][node2] = weight # A to B = 4
-        self.m_graph[node2][node1] = weight # B to A = 4 
-        
-      
-            
-      
+        # create an empty adjacency matrix with the nodes of 9 and all value equal to 0
+        self.m_graph = [[0 for column in range(num_of_nodes)]
+                        for row in range(num_of_nodes)]
+    # this is method ,used to reduce the number of edge,
+
+    def add_edge(self, node1, node2, weight):
+        # array [line][colum]
+        #i.e. m_graph [1][3] =5 /line 1 colum 3 = 5
+        self.m_graph[node1][node2] = weight  # A to B = 4
+        self.m_graph[node2][node1] = weight  # B to A = 4
+
     #this is method
+
     def prims_mst(self):
-        # we create a large number, so that edge alwasy lesser when compare
-        # flaot('inf') is mean to set the value to infinitive
+        # we create a large number, so that edge alway lesser when compare
+        # float('inf') is mean to set the value to infinitive
         postitive_inf = float('inf')
 
         # we use this to check whether nodes are chosen or not
+        # this line make all the node to False
+        # 0 : False
+        # 1 : False
+        # 2 : False
+        # 3 : False
+        # 4 : False
+        # 5 : False
+        # 6 : False
+        # 7 : False
+        # 8 : False
         selected_nodes = [False for node in range(self.m_num_of_nodes)]
 
         # we store our MST result here
-        result = [[0 for column in range(self.m_num_of_nodes)] 
-                    for row in range(self.m_num_of_nodes)]
+        result = [[0 for column in range(self.m_num_of_nodes)]
+                  for row in range(self.m_num_of_nodes)]
         # print the graph out
-        indx = 0
+        index = 0
         for i in range(self.m_num_of_nodes):
             print(self.m_graph[i])
-        # print False 
+        # print False
         print(selected_nodes)
-       
 
-        # while loop check the nodes that haven't choose
+        # Loop end when all the node aren't False
         while(False in selected_nodes):
             # assign the biggest value to minimum
             minimum = postitive_inf
@@ -48,29 +59,29 @@ class Graph:
             end = 0
 
             for i in range(self.m_num_of_nodes):
-                
+
                 if selected_nodes[i]:
                     for j in range(self.m_num_of_nodes):
                         # If the analyzed node have a path to the ending node AND its not included in the MST (to avoid cycles)
-                        if (not selected_nodes[j] and self.m_graph[i][j]>0):  
-                            # If the weight path analized is less than the minimum of the MST
+                        if (not selected_nodes[j] and self.m_graph[i][j] > 0):
+                            # If the weight path analyzed is less than the minimum of the MST
                             if self.m_graph[i][j] < minimum:
                                 # Defines the new minimum weight, the starting vertex and the ending vertex
                                 minimum = self.m_graph[i][j]
                                 start, end = i, j
-   
+
             # Since we added the ending vertex to the MST, it's already selected:
             selected_nodes[end] = True
 
             # Filling the MST Adjacency Matrix fields:
             result[start][end] = minimum
-            
+
             if minimum == postitive_inf:
                 result[start][end] = 0
 
-            #print("(%d.) %d - %d: %d" % (indx, start, end, result[start][end]))
-            indx += 1
-            
+            #print("(%d.) %d - %d: %d" % (index, start, end, result[start][end]))
+            index += 1
+
             result[end][start] = result[start][end]
 
         # Print the resulting MST
@@ -78,15 +89,13 @@ class Graph:
         for i in range(len(result)):
             for j in range(0+i, len(result)):
                 if result[i][j] != 0:
-                    print("%d - %d: %d" % (i, j, result[i][j]))       
-                
-                
-                
-                
-                
+                    print("%d - %d: %d" % (i, j, result[i][j]))
+
+
                 # Example graph has 9 nodes
+# create a object example_graph from a Graph class
 example_graph = Graph(9)
-                
+
 example_graph.add_edge(0, 1, 4)
 example_graph.add_edge(1, 0, 4)
 example_graph.add_edge(0, 2, 7)
--- test_prime.py
+++ test_prime.py
@@ -4,18 +4,32 @@
 
 # for i in range(num_of_nodes):
 #         print(m_graph[i])
+
+
+# print(m_graph[2][2])
 # i = 0
 # selected_nodes = [False for node in range(num_of_nodes)]
 # for i in range(m_num_of_nodes):
 #     print(selected_nodes)
-    # if not selected_nodes[i]:
-    #     print(m_num_of_nodes,[i],selected_nodes)
+#     if not selected_nodes[i]:
+#         print(m_num_of_nodes,[i],selected_nodes)
 
 # print(selected_nodes)
 # result = [[0 for column in range(m_num_of_nodes)] 
 #                     for row in range(m_num_of_nodes)]
 # print(result)
 
-for i in range(9):
-    for j in range(0+i, 9):
-        print(i,j)
+# for i in range(9):
+#         print(i,": False")
+#     for j in range(0+i, 9):
+#         print(i,j)
+list = [[0, 4, 7, 0, 0, 0, 0, 0, 0],
+        [4, 0, 11, 9, 0, 20, 0, 0, 0],
+        [7, 11, 0, 0, 0, 1, 0, 0, 0],
+        [0, 9, 0, 0, 2, 0, 6, 0, 0],
+        [0, 0, 0, 2, 0, 1, 10, 5, 15],
+        [0, 20, 1, 0, 1, 0, 0, 3, 0],
+        [0, 0, 0, 6, 10, 0, 0, 0, 5],
+        [0, 0, 0, 0, 5, 3, 0, 0, 12],
+        [0, 0, 0, 0, 15, 0, 5, 12, 0]]
+print(list[2][3])

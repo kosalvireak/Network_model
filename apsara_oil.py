5527f697dcc543fce162be3d6fa28546e1f3292a
Kosalvireak Khoeun <virakvary@gmail.com>
2022-08-14 22:59:00 +0700

    commit

--- apsara_oil.py
+++ apsara_oil.py
@@ -2,37 +2,58 @@
 list_of_network = []
 # value of node by index
 list_of_weight = []
-# store value to node
-list3={}
-# all node 
+# store new value to node
+list_of_new_value = {}
+# all node
 nodes = []
-
-# create a value in and value out for comparison
-i = 0
 
 
 def add_edge(node1, node2, weight):
-    global i
     node = node1, node2
-    list_of_network.insert(i, node)
-    list_of_weight.insert(i, weight)
+    # add node and its network
+    list_of_network.append(node)
+    # create a weight list the value correspond to list_of_network's index
+    list_of_weight.append(weight)
+    # list of all nodes
     if node1 not in nodes:
         nodes.append(node1)
     if node2 not in nodes:
         nodes.append(node2)
     # list.insert(i,node2)
-    i += 1
 
 
 add_edge(0, 1, 6)
 add_edge(0, 2, 4)
 add_edge(1, 3, 3)
 add_edge(1, 4, 2)
-add_edge(2, 3, 2)    
+add_edge(2, 3, 2)
 add_edge(2, 4, 5)
 add_edge(3, 5, 6)
 add_edge(4, 5, 4)
 
+print(nodes)
 print(list_of_network)
 print(list_of_weight)
-print(nodes)
+print("------------")
+
+
+# create a value in and value out for comparison
+j = 0
+while j <= (len(list_of_network)):
+    for i in range (len(list_of_network)):
+        # check the weight
+        # i = 0 , (0,1) = 5 add value 5 to key 1 in list_of_new_value
+
+        if list_of_network[i][0] == j:
+            list_of_new_value[list_of_network[i][1]] = list_of_weight[i]
+    j = j + 1 
+
+#print(list_of_new_value)
+
+#check all the network រកទីតាំងដើម ដើម្បីផ្តល់តម្លៃទៅទីតាំងបញ្ចប់
+list_of_new={}
+for nod in nodes:
+    for i in range (len(list_of_network)):
+        if list_of_network[i][0] == nod:
+            list_of_new[list_of_network[i][1]] = list_of_weight[i]
+print(list_of_new)

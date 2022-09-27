da351a98f03c092690ee6ec3f5744863991a90fe
Kosalvireak Khoeun <virakvary@gmail.com>
2022-09-27 11:11:19 +0700

    key 3 got only 2 value

--- algorithm_bases.py
+++ algorithm_bases.py
@@ -33,8 +33,6 @@ add_edge(4, 5, 4)
 
 print("Node", Node)
 print("Network", Network)
-print("success_dic", success_dic)
-print("------------")
 
 # Assign value to node that receive from node0
 for i in range(len(Network)):
@@ -45,51 +43,55 @@ for i in range(len(Network)):
         new_value = Network[i][2]
         success_dic[new_node] = new_value
 print("success_dic", success_dic)
-temp_list = []
+
 
 
 # --> need a big loop
 
+for m in range(1, 3):
+    print("------------")
+    print(m)
+    print("------------")
+    temp_list = []
+    for i in range(len(Network)):
+        # Assign Network to temp list
+        if Network[i][0] == m:
+            node = Network[i][0], Network[i][1], Network[i][2]
+            temp_list.append(node)
+        # clear temp_list
+        # temp_list.clear()
+    print("temp_list", temp_list)
 
-for i in range(len(Network)):
-    # Assign Network to temp list
-    if Network[i][0] == 1:
-        node = Network[i][0], Network[i][1], Network[i][2]
-        temp_list.append(node)
-    # clear temp_list
-    # temp_list.clear()
-print("temp_list", temp_list)
+    # This is for temp_dic if it equal = 2
+    # Compare 3rd elements of both
+    smallest = 100
+    biggest = 0
+    if temp_list[0][2] < temp_list[1][2]:
+        smallest = temp_list[0][2]
+        smallest_key = temp_list[0][1]
+        biggest = temp_list[1][2]
+        biggest_key = temp_list[1][1]
+    elif temp_list[1][2] < temp_list[0][2]:
+        smallest = temp_list[1][2]
+        smallest_key = temp_list[1][1]
+        biggest = temp_list[0][2]
+        biggest_key = temp_list[0][1]
+    # print(smallest,smallest_key)
+    # print(biggest,biggest_key)
 
-print("------------")
-# This is for temp_dic if it equal = 2
-# Compare 3rd elements of both
-smallest = 100
-biggest = 0
-if temp_list[0][2] < temp_list[1][2]:
-    smallest = temp_list[0][2]
-    smallest_key = temp_list[0][1]
-    biggest = temp_list[1][2]
-    biggest_key = temp_list[1][1]
-elif temp_list[1][2] < temp_list[0][2]:
-    smallest = temp_list[1][2]
-    smallest_key = temp_list[1][1]
-    biggest = temp_list[0][2]
-    biggest_key = temp_list[0][1]
-# print(smallest,smallest_key)
-# print(biggest,biggest_key)
+    # assign smallest to success_dic
+    success_dic[smallest_key] = smallest
+    success_dic[temp_list[0][0]] = success_dic[temp_list[0][0]] - smallest
 
-# assign smallest to success_dic
-success_dic[smallest_key] = smallest
-success_dic[temp_list[0][0]] = success_dic[temp_list[0][0]] - smallest
+    # Compare its weight and the value of nod (1,3,3) weight is 3 and value of nod is 4
+    if biggest < success_dic[temp_list[0][0]]:
+        success_dic[biggest_key] += biggest
+        success_dic[temp_list[0][0]] = success_dic[temp_list[0][0]] - biggest
+    elif biggest > success_dic[temp_list[0][0]]:
+        success_dic[biggest_key] += success_dic[temp_list[0][0]]
 
-# Compare its weight and the value of nod (1,3,3) weight is 3 and value of nod is 4
-if biggest < success_dic[temp_list[0][0]]:
-    success_dic[biggest_key] = success_dic[biggest_key] + biggest
-    success_dic[temp_list[0][0]] = success_dic[temp_list[0][0]] - biggest
-elif biggest > success_dic[temp_list[0][0]]:
-    success_dic[biggest_key] = success_dic[biggest_key] + \
-        success_dic[temp_list[0][0]]
+        success_dic[temp_list[0][0]] -= success_dic[temp_list[0][0]]
 
-    success_dic[temp_list[0][0]] -= success_dic[temp_list[0][0]]
-
+    print(success_dic)
+print("============")
 print(success_dic)
